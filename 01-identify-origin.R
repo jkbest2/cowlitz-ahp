4cf2623294c4e7ecbb66fd5d6bdf4b08bbe12622
John K Best <John.Best@dfw.wa.gov>
2025-07-18 15:13:58 -0700

    Add function to identify fish origin based on species and tag

--- 01-identify-origin.R
+++ 01-identify-origin.R
@@ -1,12 +1,15 @@
-origin_sprchk <- function(tag, lifestage) {
-  if (lifestage == "Mini Jacks") {
-    orgn <- "Hatchery (HOR)"
-  } else {
-    orgn <- ifelse(tag == "UM", "Wild (NOR)", "Hatchery (HOR)")
-  }
+origin_sprchk <- function(tag) {
+  orgn <- case_match(
+    tag,
+    c(
+      "AD + CWT", "AD + RV", "AD + RV + OP RET",
+      "Ad only", "UM + BWT"
+    ) ~ "Hatchery (HOR)",
+    c("UM", "UM + PIT Tag", "OP RET") ~ "Wild (NOR)"
+  )
   factor(orgn, levels = c("Hatchery (HOR)", "Integrated (H/W)", "Wild (NOR)"))
 }
-  
+
 origin_fallchk <- function(tag) {
   orgn <- case_match(
     tag,
@@ -35,10 +38,32 @@ origin_wintsth <- function(tag) {
   orgn <- case_match(
     tag,
     "Ad only" ~ "Hatchery (HOR)",
-    c("AD + CWT", "")
+    c("AD + CWT", "AD + LV", "AD+LV+PIT") ~ "Integrated (H/W)",
+    c(
+      "UM", "UM + BWT", "UM + BWT + Pit",
+      "UM + DOR + PIT", "UM + DORSAL CWT",
+      "UM + PIT Tag", "UM + RV"
+    ) ~ "Wild (NOR)"
   )
+  factor(orgn, levels = c("Hatchery (HOR)", "Integrated (H/W)", "Wild (NOR)"))
+}
+
+origin_summsth <- function(tag) {
+  orgn <- case_match(
+    tag,
+    c("Ad only", "AD + OP + PIT", "AD + PIT Tag") ~ "Hatchery (HOR)",
+    c("UM", "OP RET") ~ "Wild (NOR)"
+  )
+  factor(orgn, levels = c("Hatchery (HOR)", "Integrated (H/W)", "Wild (NOR)"))
 }
 
 id_origin <- function(species, tag) {
-
+  case_match(
+    species,
+    "Spring Chinook" ~ origin_sprchk(tag),
+    "Fall Chinook" ~ origin_fallchk(tag),
+    "Coho" ~ origin_coho(tag),
+    "Stlhd Winter" ~ origin_wintsth(tag),
+    "Stlhd Summer" ~ origin_summsth(tag)
+  )
 }
